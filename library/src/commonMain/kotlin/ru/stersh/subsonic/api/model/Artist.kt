8c2852210a5a597917483c6acb303afcb1966ae2
Kirill Zhukov <siper13@gmail.com>
2025-02-26 18:22:11 +0300

    Fix artists response

--- library/src/commonMain/kotlin/ru/stersh/subsonic/api/model/Artist.kt
+++ library/src/commonMain/kotlin/ru/stersh/subsonic/api/model/Artist.kt
@@ -10,7 +10,7 @@ data class ArtistsResponse(
 @Serializable
 data class Artists(
     val ignoredArticles: String,
-    val indices: List<Index>,
+    val index: List<Index>,
 ) {
 
     @Serializable
